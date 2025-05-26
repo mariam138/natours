59765f62655953f51c02df6be26d0eb96692992c
Mariam <mar.ayoub98@gmail.com>
2025-05-26 16:33:32 +0400

    refactor: create function to get all tours

--- app.js
+++ app.js
@@ -5,25 +5,11 @@ const app = express();
 //////////////////////////////////////////////////////////////////////////////// MIDDLEWARE
 app.use(express.json());
 
-// // Getting the root URL
-// app.get("/", (req, res) => {
-//   res
-//     .status(200)
-//     // using json method automatically defines content type as json
-//     .json({ message: "Hello from the server side!", app: "Natours" });
-// });
-
-// app.post("/", (req, res) => {
-//   res.send("You can post to this endpoint...");
-// });
-
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-// callback is called the route handler
-//////////////////////////////////////////////////////////////////////////////// GET
-app.get("/api/v1/tours", (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     // Format data in JSend data specification
     status: "success",
@@ -33,7 +19,10 @@ app.get("/api/v1/tours", (req, res) => {
       tours,
     },
   });
-});
+};
+
+//////////////////////////////////////////////////////////////////////////////// GET
+app.get("/api/v1/tours", getAllTours);
 
 //////////////////////////////////////////////////////////////////////////////// GET by id
 app.get("/api/v1/tours/:id", (req, res) => {
