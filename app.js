50e50bdc5d1542336ebb38bcc09488bbe7e8a9ca
Mariam <mar.ayoub98@gmail.com>
2025-05-29 15:49:46 +0400

    create route handlers for users resources

--- app.js
+++ app.js
@@ -118,6 +118,41 @@ const deleteTour = (req, res) => {
   });
 };
 
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined",
+  });
+};
+
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined",
+  });
+};
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined",
+  });
+};
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined",
+  });
+};
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined",
+  });
+};
+
 //////////////////////////////////////////////////////////////////////////////// ROUTES
 
 // app.get("/api/v1/tours", getAllTours);
