d9f0d34162f360f2b3821c3088020a4529164159
Mariam <mar.ayoub98@gmail.com>
2025-06-10 13:12:41 +0400

    import tour controllers into routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,11 +1,12 @@
 const fs = require("fs");
 const express = require("express");
+const tourController = require("../controllers/tourController");
 
 // Middleware for the tour router - no longer need to specify the router as the router is in it's own file
 const router = express.Router();
 
 // Tour routes
-router.route("/").get(getAllTours).post(createTour);
-router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
+router.route("/").get(tourController.getAllTours).post(tourController.createTour);
+router.route("/:id").get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
 
 module.exports = router;
