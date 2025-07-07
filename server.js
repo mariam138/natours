4f6e65f013a1677348ad555b9591c43ef4a5ca8c
Mariam <mar.ayoub98@gmail.com>
2025-07-07 14:02:01 +0100

    setup mongoose driver for mongodb

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 // Reads variables in config.env and add them to node environment variables
 dotenv.config({ path: "./config.env" });
 
 const app = require("./app");
+
+// Mongoose Driver Setup
+const DB = process.env.DATABASE.replace(
+  "<db_password>",
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindandModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log("DB connection successful");
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
