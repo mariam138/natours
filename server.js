e4252e8cb2b7c558f1fd042b5b212d3704bc4899
Mariam <mar.ayoub98@gmail.com>
2025-07-25 12:49:19 +0100

    remove connection log

--- server.js
+++ server.js
@@ -11,10 +11,7 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD,
 );
 
-mongoose.connect(DB).then((con) => {
-  console.log(con.connections);
-  console.log("DB connection successful");
-});
+mongoose.connect(DB).then(() => console.log("DB connection successful"));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
